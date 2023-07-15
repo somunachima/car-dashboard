0f74938991e09b83e31457bab92add6dd13166f4
Som Akudu <somakudu@gmail.com>
2023-07-15 11:59:32 +0100

    fix API to show images of cars and start adding styling and HTML structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import Images from "./images.json";
 
-const App = () => {
-  const [images, setImages] = useState();
-
-  useEffect(() => {
-    fetch('images?limit=10')
-      .then(res => res.json())
-      .then(data => {
-        console.log('Success:', data);
-        setImages(data);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
-  }, []);
+function App() {
 
   return (
     <div className='app'>
-      <h2>Images</h2>
+      <div className="navbar">
+        <img className="home-logo" src="../logo192.png"/>
+        <div className='menu'>
+          <p>Home</p>
+          <p>About</p>
+          <p>Cars</p>
+        </div>
+        <button>Contact Us</button>
+      </div>
+
+      <div className='hero-page'>
+
+      </div>
+
       {
-        images && images.map(img => (
-          <div key={img.id} >
-            <img src={`${img.url}.jpg`} alt=''/>
-            <img src={`${img.user.profile_image}.webp`} alt=''/>
+        Images && Images.map(img => (
+          <div key={img.id} className='car-card' >
+            <img className="car-img" src={`${img.url}.jpg`} alt=''/>
+            <img className="hero-img" src={`${img.user.profile_image}.webp`} alt=''/>
           </div>
         ))
       }
