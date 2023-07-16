9fb2eab90dde6cf39df2e0f81c84b760a4aad937
Som Akudu <somakudu@gmail.com>
2023-07-16 23:25:19 +0100

    add backend to the search bar so that cars can be searched by colour by looking through alt_description for each car

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import RevealOnScroll from "@phntms/reveal-on-scroll";
+import { Zoom } from "react-awesome-reveal";
 import ModalImage from "react-modal-image";
 import './App.css';
 import Images from "./images.json";
-import Popup from './Popup';
-import SignUp from './SignUp';
+import Popup from './components/Popup';
+import SignUp from './components/SignUp';
 import { v4 as uuidv4 } from 'uuid';
 import Swal from "sweetalert2";
 
@@ -12,6 +12,7 @@ import Swal from "sweetalert2";
 function App() {
   const [buttonPopup, setButtonPopup] = useState(false);
   const [signs, setSigns] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const signUp = (sign) => {
     const id = uuidv4();
@@ -25,55 +26,67 @@ function App() {
     localStorage.setItem("Signed up!", JSON.stringify([...signs, newSign]));
   }
 
-  new RevealOnScroll();
-
   return (
-    <div className='app'>
-      <div className="navbar">
-        <img className="home-logo" src="../logo192.png" alt="logo"/>
-        <a className="contact-btn" href="mailto:som@motorway.com?subject=Hello!">Contact Us</a>
-      </div>
+    <>
+      <div className='app'>
+        <div className="navbar">
+          <img className="home-logo" src="../logo192.png" alt="logo"/>
+          <a className="contact-btn" href="mailto:som@motorway.com?subject=Hello!">Contact Us</a>
+        </div>
 
-      <div className='hero-page'>
-        <h1><strong><i>Find the best car for you.</i></strong></h1>
-        <button className="sign-btn" onClick={() => setButtonPopup(true)}>Sign Up</button>
-        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-          <h6>Sign Up</h6>
-          <span className="task-type">
-            <SignUp onSave={signUp}/>
-          </span>
-        </Popup>
-      </div>
-      <h3 className='search-title'><strong><i>Read what people are saying about your car of choice</i></strong></h3>
+        <div className='hero-page'>
+          <h1><strong><i>Find the best car for you.</i></strong></h1>
+          <button className="sign-btn" onClick={() => setButtonPopup(true)}>Sign Up</button>
+          <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+            <h6>Sign Up</h6>
+            <span className="task-type">
+              <SignUp onSave={signUp}/>
+            </span>
+          </Popup>
+        </div>
+        <h3 className='search-title'><strong><i>Read what people are saying about your car of choice</i></strong></h3>
 
-      <div className='search'>
-        <i className="fa-solid fa-magnifying-glass search-icon"></i>
-        <input className="search-bar" id="input" name="name" type="text" placeholder="Search cars by colour" autoFocus></input>
-      </div>
-      <div id="display-data"></div>
+        <div className='search'>
+          <i className="fa-solid fa-magnifying-glass search-icon"></i>
+          <input className="search-bar" id="input" name="name" type="text" placeholder="Search cars by colour" onChange={(event) => {
+              setSearchTerm(event.target.value);
+            }}/>
+        </div>
 
-      {
-        Images && Images.map(img => (
-          <div key={img.id} className='car-card' >
-            <div className="top-card">
-              <div className="profile">
-                <img className="hero-img" src={`${img.user.profile_image}.webp`} alt=''/>
-                <p className="name"><strong>{`${img.user.name}`}</strong></p>
+        {
+          Images && Images
+          .filter((img) => {
+            if(searchTerm === ""){
+              return img;
+            }else if(img.alt_description.toLowerCase().includes(searchTerm.toLowerCase())){
+              return img;
+            }
+          })
+
+          .map((img) => {
+            return(
+            <Zoom key={img.id} className='car-card' >
+              <div className="top-card">
+                <div className="profile">
+                  <img className="hero-img" src={`${img.user.profile_image}.webp`} alt=''/>
+                  <p className="name"><strong>{`${img.user.name}`}</strong></p>
+                </div>
+                <ModalImage
+                    className="car-img"
+                    small={`${img.url}.jpg`}
+                    large={`${img.url}.jpg`}
+                    alt=""
+                />;
+                <div className="card-bottom">
+                  <p className="bio">{`${img.user.bio}`}</p>
+                </div>
               </div>
-              <ModalImage
-                  className="car-img"
-                  small={`${img.url}.jpg`}
-                  large={`${img.url}.jpg`}
-                  alt=""
-              />;
-              <div className="card-right">
-                <p className="bio">{`${img.user.bio}`}</p>
-              </div>
-            </div>
-          </div>
-        ))
-      }
-    </div>
+            </Zoom>
+            )
+          })
+        }
+      </div>
+    </>
   );
 }
 
